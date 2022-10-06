ca05d83ad5d6e91ddb7a7cebf0d87365bdf78860
dhirajberi.official@gmail.com <91799142+dhirajberiyudiz@users.noreply.github.com>
2022-10-06 14:48:32 +0530

    add cors and index page

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 import collections
 
 app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
 
 @app.route("/input-board", methods=['POST'])
 def input_board():
